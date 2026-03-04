7f436a6ea28eb351d13f4e7a49016a086b180dae
Ubuntu <ubuntu@ip-172-31-26-42.ap-south-1.compute.internal>
2026-03-04 09:18:55 +0000

    changes in app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Running Successfully on 04/03/2026 by sahilmahat"
+    return "CI/CD Pipeline Running Successfully on 04/03/2026 by sahilmahat 
+                hey how are you`"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
